4b970c73afa4f36e95ec4dc173891804bbe1468a
Ragima Mammadaliyeva <ragimamammadaliyeva@Ragimas-Laptop.local>
2024-08-22 21:53:59 +0400

    Add Error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,6 +1,22 @@
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import Section from "../../components/Section/Section";
 import "./error-page.scss";
 
 const ErrorPage = () => {
-  return <>Error</>;
+  return (
+    <>
+      <Header />
+      <main className="error-page">
+        <Section className={"error-heading-section"}>
+          <h1>Page Not Found</h1>
+        </Section>
+        <Section className={"error-section"}>
+          <div className="not-found">404</div>
+        </Section>
+      </main>
+      <Footer />
+    </>
+  );
 };
 export default ErrorPage;
